d6553d8d93521b5077c6aa089acae413e29daf4a
javier <devjavicont@gmail.com>
2021-11-14 14:55:59 -0400

    se creo y se importo la pagina 404

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import "../styles/styles.scss"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import AllSettings from "./pages/allSettings"
+import Page404 from "./pages/page404"
+
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="settings" element={<AllSettings/>}/>
+      <Route path="*" element={<Page404/>}/>
     </Routes>
   </BrowserRouter>
 )
