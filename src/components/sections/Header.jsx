00e8f8116ec06d31e94d62b25bebcc276b88d286
javier <devjavicont@gmail.com>
2021-11-14 18:08:41 -0400

    se importo "Search", "MoreOptions" y se agrego clases los elementos

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,10 +1,17 @@
 import { Link } from "react-router-dom"
+import MoreOptions from "../fragments/MoreOptions"
+import Search from "../fragments/Search"
 
 const Header = () => {
   return (
-    <header>
-      <Link to="settings">usuario</Link>
-      <h1>Signal</h1>
+    <header className="home-header">
+      <Link className="user-icon" to="settings">
+        <img src="" alt="" />
+        <span>icon</span>
+      </Link>
+      <h1 className="title">Signal</h1>
+      <Search/>
+      <MoreOptions/>
     </header>
   )
 }
