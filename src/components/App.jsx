bb065f7ebee9baf9ecfb3e1b6e272e62ae41c0e8
javier <devjavicont@gmail.com>
2021-11-13 20:09:15 -0400

    se importaron las rutas de react y las paginas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,15 @@
 import "../styles/styles.scss"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/home"
+import AllSettings from "./pages/allSettings"
 
-const App = () => null
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="settings" element={<AllSettings/>}/>
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
