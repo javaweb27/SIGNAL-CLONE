abc5455016381aeae400c02de1b4be1022525912
javier <devjavicont@gmail.com>
2021-11-15 19:50:18 -0400

    se agreo enlaces a la seccion de ajustes y se importo el icono del usuario

--- src/components/sections/Settings.jsx
+++ src/components/sections/Settings.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import UserIcon from "../../../public/UserIcon"
 
 const Settings = () => {
   return (
@@ -10,8 +11,21 @@ const Settings = () => {
         <h2>Ajustes</h2>
       </header>
 
-      <section>
-        varios ajustes
+      <section className="settings">
+        <Link to="profile" className="link">Usuario</Link>
+
+        <Link to="account" className="link"><UserIcon/>Cuenta</Link>
+        <Link to="linked-devices" className="link"><UserIcon/>Dispositivos enlazados</Link>
+        <hr />
+        <Link to="appearance" className="link"><UserIcon/>Apariencia</Link>
+        <Link to="chats" className="link"><UserIcon/>Chats</Link>
+        <Link to="notifications" className="link"><UserIcon/>Notificaciones</Link>
+        <Link to="privacy" className="link"><UserIcon/>Privacidad</Link>
+        <Link to="data" className="link"><UserIcon/>Datos y almacenamiento</Link>
+        <hr />
+        <Link to="help" className="link"><UserIcon/>Ayuda</Link>
+        <Link to="invite" className="link"><UserIcon/>¡Invita a tus amistades!</Link>
+        <Link to="support-signal" className="link"><UserIcon/>Donar a Signal</Link>
       </section>
     </>
   )
