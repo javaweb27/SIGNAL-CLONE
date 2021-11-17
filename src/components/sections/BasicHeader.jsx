bbf2e24c269d24db6ce55491997c1297e0378c31
javier <devjavicont@gmail.com>
2021-11-17 18:00:20 -0400

    se creo el componente de header para una subpagina

---
+++ src/components/sections/BasicHeader.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+import ArrowIcon from "../../svg/ArrowIcon"
+
+const BasicHeader = ({linkPath, titleType, titleText}) => (
+  <header className="basic-header">
+    <Link to={linkPath}><ArrowIcon/></Link>
+    {titleType==="h2" && <h2 className="title">{titleText}</h2>}
+    {titleType==="h3" && <h3 className="title">{titleText}</h3>}
+    {titleType==="h4" && <h4 className="title">{titleText}</h4>}
+  </header>
+)
+
+export default BasicHeader
