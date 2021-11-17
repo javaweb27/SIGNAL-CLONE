6d7dcf496404c81f79c3e3a1f01f79e87a1e942a
javier <devjavicont@gmail.com>
2021-11-17 19:52:12 -0400

    Se importo el perfil del usuario al header del home

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,14 +1,12 @@
 import { Link } from "react-router-dom"
 import MoreOptions from "../fragments/MoreOptions"
 import Search from "../fragments/Search"
+import UserProfile from "../fragments/UserProfile"
 
 const Header = () => {
   return (
     <header className="home-header">
-      <Link className="user-icon" to="settings">
-        <img src="" alt="" />
-        <span>icon</span>
-      </Link>
+      <UserProfile forHeader/>
       <h1 className="title">Signal</h1>
       <Search/>
       <MoreOptions/>
