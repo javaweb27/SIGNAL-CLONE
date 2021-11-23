353560eb59ea0d9759f0b1de62651bcc42bc305f
javier <devjavicont@gmail.com>
2021-11-23 19:27:37 -0400

    se creo el icono de notificaciones con clases para los temas

---
+++ src/svg/NotificationsIcon.jsx
@@ -0,0 +1,8 @@
+const NotificationsIcon = () => (
+  <svg width="60" height="60" version="1.1" viewBox="0 0 15.875 15.875">
+    <path className="text-color-stroke-for-dark-theme text-color-fill-for-dark-theme" d="m1.3055 13.04c-0.60992 0.011196-0.96559-1.0157-0.94713-1.3918 0.07423-1.5123 1.3753-0.90444 2.2354-1.817 0.86009-0.91254 1.3463-5.7492 2.0764-7.8022 0.60704-1.7071 3.2674-1.672 3.2674-1.672s2.6604-0.035039 3.2674 1.672c0.73004 2.053 1.2163 6.8897 2.0764 7.8022 0.8601 0.91254 2.1612 0.30465 2.2354 1.817 0.01845 0.37609-0.33721 1.403-0.94713 1.3918h-6.632z" fill="none" stroke="#000" strokeLinejoin="round" strokeWidth=".71444"/>
+    <path className="text-color-fill-for-dark-theme" d="m5.8536 14.422h4.1679s-0.59743 1.4528-2.0399 1.4528c-1.4425 0-2.128-1.4528-2.128-1.4528z"/>
+  </svg>
+)
+
+export default NotificationsIcon
