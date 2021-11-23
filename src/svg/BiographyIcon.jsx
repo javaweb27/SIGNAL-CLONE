d75c1452adb0b77f9ecd467b1034122a7564acc2
javier <devjavicont@gmail.com>
2021-11-23 19:28:37 -0400

    se creo el icono de Biografia con clases para los temas

---
+++ src/svg/BiographyIcon.jsx
@@ -0,0 +1,13 @@
+const BiographyIcon = () => (
+  <svg width="60" height="60" version="1.1" viewBox="0 0 15.875 15.875">
+    <g transform="matrix(.56515 .5651 -.6375 .63744 8.5529 -3.1646)">
+      <path className="bg-color-fill-for-light-theme text-color-fill-for-dark-theme" d="m9.1644 20.075c-0.058279-0.091778-0.47269-0.80874-0.92092-1.5932l-0.81497-1.4264 0.0048-12.362 3.6646-0.00256 7.06e-4 6.1888 7.09e-4 6.1888-0.8576 1.4889c-0.47168 0.81892-0.88318 1.5329-0.91446 1.5866-0.04288 0.0737-0.08283 0.05681-0.16283-0.0692z"/>
+      <path className="bg-color-fill-for-light-theme text-color-fill-for-dark-theme" d="m9.4586 3.9807-2.0264-1.126e-4 0.012061-2.2148 0.012059-2.244 3.9354-0.00267 0.04678 0.5748c0.02573 0.31614 0.04672 1.3267 0.04663 2.2458l-1.61e-4 1.6412z"/>
+      <path className="text-color-fill-for-dark-theme" d="m6.8256 4.6907s6e-7 8.2167-2e-7 12.325l2.4523 4.6257 2.4497-4.6257v-12.325l-0.70698-7e-7 -4.5e-5 12.146-1.7448 3.2934-1.7427-3.2934c-6.46e-5 -4.0486-5.227e-4 -12.146-5.227e-4 -12.146z" color="#000000" strokeWidth=".99359"/>
+      <path className="text-color-fill-for-dark-theme" d="m6.825-1.086-5e-7 0.35626-2e-7 4.7102 0.70654-4e-7 -3e-7 -3.2445-1e-7 -0.60991 5.116e-4 -3.1e-7 -3e-7 -0.49907 3.4882-4e-7v0.49907l5.11e-4 3.5e-7v3.8544l0.70654-3e-7v-5.0665z" color="#000000" strokeWidth=".99355" />
+      <path className="bg-color-stroke-for-dark-theme" d="m6.825 4.3325 4.9025-4.63e-5" fill="none" stroke="#000" strokeWidth=".79218"/>
+    </g>
+  </svg>
+)
+
+export default BiographyIcon
