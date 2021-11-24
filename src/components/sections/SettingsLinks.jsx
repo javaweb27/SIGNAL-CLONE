739bf0221f17df7d6737569130856d3d3a32c08c
javier <devjavicont@gmail.com>
2021-11-23 21:30:24 -0400

    se importo el icono de Chats, Apariencia y Notificaciones

--- src/components/sections/SettingsLinks.jsx
+++ src/components/sections/SettingsLinks.jsx
@@ -2,6 +2,9 @@ import { Link } from "react-router-dom"
 import UserProfile from "../fragments/UserProfile"
 import UserIcon from "../../svg/UserIcon"
 import LinkedDevicesIcon from "../../svg/LinkedDevicesIcon"
+import ChatsIcon from "../../svg/ChatsIcon"
+import AppearanceIcon from "../../svg/AppearanceIcon"
+import NotificationsIcon from "../../svg/NotificationsIcon"
 
 const SettingsLinks = () => {
   return (
@@ -11,9 +14,9 @@ const SettingsLinks = () => {
       <Link to="account" className="link"><UserIcon/>Cuenta</Link>
       <Link to="linked-devices" className="link"><LinkedDevicesIcon/>Dispositivos enlazados</Link>
       <hr />
-      <Link to="appearance" className="link"><UserIcon/>Apariencia</Link>
-      <Link to="chats" className="link"><UserIcon/>Chats</Link>
-      <Link to="notifications" className="link"><UserIcon/>Notificaciones</Link>
+      <Link to="appearance" className="link"><AppearanceIcon/>Apariencia</Link>
+      <Link to="chats" className="link"><ChatsIcon/>Chats</Link>
+      <Link to="notifications" className="link"><NotificationsIcon/>Notificaciones</Link>
       <Link to="privacy" className="link"><UserIcon/>Privacidad</Link>
       <Link to="data" className="link"><UserIcon/>Datos y almacenamiento</Link>
       <hr />
