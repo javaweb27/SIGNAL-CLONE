9358e2bc61340614c847e4de0bf82dc6ff2cb64d
javier <devjavicont@gmail.com>
2021-11-25 18:50:16 -0400

    se importo el proveedor del contexto Apariencia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import Settings from "./pages/settings"
 import Page404 from "./pages/page404"
+//context
+import AppearanceProvider from "./context/appearance/AppearanceProvider"
 //subpages
 import Profile from "./subpages/profile/Profile"
 import Account from "./subpages/account/Account"
@@ -13,20 +15,22 @@ import Notifications from "./subpages/notifications/Notifications"
 import Privacy from "./subpages/privacy/Privacy"
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="settings" element={<Settings/>}/>
-      <Route path="settings/profile" element={<Profile/>}/>
-      <Route path="settings/account" element={<Account/>}/>
-      <Route path="settings/linked-devices" element={<LinkedDevices/>}/>
-      <Route path="settings/appearance" element={<Appearance/>}/>
-      <Route path="settings/chats" element={<ChatsSettings/>}/>
-      <Route path="settings/notifications" element={<Notifications/>}/>
-      <Route path="settings/privacy" element={<Privacy/>}/>
-      <Route path="*" element={<Page404/>}/>
-    </Routes>
-  </BrowserRouter>
+  <AppearanceProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="settings" element={<Settings/>}/>
+        <Route path="settings/profile" element={<Profile/>}/>
+        <Route path="settings/account" element={<Account/>}/>
+        <Route path="settings/linked-devices" element={<LinkedDevices/>}/>
+        <Route path="settings/appearance" element={<Appearance/>}/>
+        <Route path="settings/chats" element={<ChatsSettings/>}/>
+        <Route path="settings/notifications" element={<Notifications/>}/>
+        <Route path="settings/privacy" element={<Privacy/>}/>
+        <Route path="*" element={<Page404/>}/>
+      </Routes>
+    </BrowserRouter>
+  </AppearanceProvider>
 )
 
 export default App
