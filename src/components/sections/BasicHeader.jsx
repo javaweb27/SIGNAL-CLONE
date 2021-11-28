7185b8a9f6247248df4a7a94f42b3f3556f9e671
javier <devjavicont@gmail.com>
2021-11-28 19:13:37 -0400

    se agrego estilos al header basico y al link de retroceder

--- src/components/sections/BasicHeader.jsx
+++ src/components/sections/BasicHeader.jsx
@@ -3,7 +3,7 @@ import ArrowIcon from "../../svg/ArrowIcon"
 
 const BasicHeader = ({linkPath, titleType, titleText}) => (
   <header className="basic-header">
-    <Link to={linkPath}><ArrowIcon/></Link>
+    <Link to={linkPath} className="link"><ArrowIcon/></Link>
     {titleType==="h2" && <h2 className="title">{titleText}</h2>}
     {titleType==="h3" && <h3 className="title">{titleText}</h3>}
     {titleType==="h4" && <h4 className="title">{titleText}</h4>}
