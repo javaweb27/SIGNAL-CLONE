48edcef3119b13233824cbc296b600b67231a46d
javier <devjavicont@gmail.com>
2022-02-20 20:24:17 -0400

    UserProfile: sus estilos ahora son css module

--- src/components/fragments/UserProfile.jsx
+++ src/components/fragments/UserProfile.jsx
@@ -1,13 +1,15 @@
+import { container, title, subtitle } from "../../styles/fragments/user-profile.module.scss"
+import classes from "../../styles/fragments/settings-link.module.scss"
 import { Link } from "react-router-dom"
 import ProfilePhoto from "./ProfilePhoto"
 
 const UserProfile = () => {
   return (
-    <Link to="profile" className="link user-profile">
+    <Link to="profile" className={classes.link}>
       <ProfilePhoto/>
-      <div className="container">
-        <span>Usuario</span>
-        <span>123456789</span>
+      <div className={container}>
+        <span className={title}>Usuario</span>
+        <span className={subtitle}>123456789</span>
       </div>
     </Link>
   )
