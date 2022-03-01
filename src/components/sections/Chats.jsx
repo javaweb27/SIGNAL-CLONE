a1b02d30697db07dc35cb527a82818a3032f4c64
javier <devjavicont@gmail.com>
2022-03-01 19:42:26 -0400

    Chats: botones de la esquina inferior agregados

--- src/components/sections/Chats.jsx
+++ src/components/sections/Chats.jsx
@@ -1,8 +1,13 @@
+import HeaderButton from "../fragments/HeaderButton"
+
 const Chats = () => {
   return (
     <section>
       <p className="message-no-chats">Todavia no hay chats. <br/> Comienza a chatear con tus amistades.</p>
-
+      <div className="buttons-container">
+        <div className="chat-btn c-gray"><HeaderButton iconName="camera"/></div>
+        <div className="chat-btn c-primary"><HeaderButton to="new-message" iconName="biography"/></div>
+      </div>
     </section>
   )
 }
