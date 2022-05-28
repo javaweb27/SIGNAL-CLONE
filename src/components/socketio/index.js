fd1e04c864dd881adacefbec88f58f82d048d1e7
javier <devjavicont@gmail.com>
2022-05-28 17:08:50 -0400

    Agrega condicion para api local y cambia el mensaje al conectarse a socket.io

--- src/components/socketio/index.js
+++ src/components/socketio/index.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client"
 
-const socketio = io(import.meta.env.VITE_NODE_API);
+const socketio = io(import.meta.env.VITE_NODE_API || "http://localhost:3060");
 socketio.on("connect", () => {
-  console.log("conectado");
+  console.log("socket.io connect");
 })
 
 export default socketio
