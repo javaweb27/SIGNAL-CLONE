2830f2727a44fc234aaf0bd91ee92d330df6067f
javier <devjavicont@gmail.com>
2022-07-28 17:56:41 -0400

    Cambia el titulo y la descripcion de la pagina register

--- src/components/pages/login-register/langRegister.js
+++ src/components/pages/login-register/langRegister.js
@@ -1,11 +1,11 @@
 export const langRegister = {
   title: {
-    spanish: "Ingresa tu correo electrónico para comenzar",
-    english: "Enter your email to get started"
+    spanish: "Ingresa tu correo electrónico para comenzar o continua como invitado",
+    english: "Enter your email to get started or continue as a guest"
   },
   par: {
-    spanish: "Continuar como invitado.",
-    english: "Continue as guest."
+    spanish: "Tu cuenta será eliminada 2 minutos después de crearse. La conexión podría no ser rápida, por favor sea paciente.",
+    english: "Your account will be deleted 2 minutes after it is created. The connection may not be fast, please be patient."
   },
   btnGuest: {
     spanish: "Continuar como invitado",
