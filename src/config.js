9b8c52272a7edc4f74acd6220a0481ff80341db1
javier <devjavicont@gmail.com>
2022-08-03 16:32:19 -0400

    Cambia la url por defecto de CORS_ORIGIN_URL

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config()
 export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/test"
 export const PORT = process.env.PORT || 3060
 export const TOKEN_KEY = process.env.TOKEN_KEY
-export const CORS_ORIGIN_URL = process.env.CORS_ORIGIN_URL || "http://localhost:3000"
+export const CORS_ORIGIN_URL = process.env.CORS_ORIGIN_URL || "http://localhost:5173"
