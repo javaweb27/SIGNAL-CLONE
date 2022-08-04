79942fb4fd7d4a71ab149ce145da518c1cf7f3c2
javier <devjavicont@gmail.com>
2022-08-04 19:26:28 -0400

    Cambia la estructura de los datos de cada chat y envia el id del emisor

--- src/sockets.js
+++ src/sockets.js
@@ -1,9 +1,19 @@
 export default (io) => {
   io.on("connection", (socket) => {
+
     console.log("A user has been connected")
 
-    socket.on("client:chat", (name, message) => {
-      io.emit("server:chat", name, message)
+    //message is an object with "body" and "from" props
+    //ignore "from" prop and use socket.id 
+    socket.on("client:chat", (message) => {
+      io.emit("server:chat", {
+        from: socket.id,
+        body: message.body
+      })
     })
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected')
+    });
   })
 }
